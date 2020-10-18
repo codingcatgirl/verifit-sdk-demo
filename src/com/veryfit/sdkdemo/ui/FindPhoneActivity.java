f9c1f21fbdc2732cac01f7cdb5e69bae1bbfa91d
Laura Klünder <laura@codingcatgirl.de>
2020-10-18 15:43:18 +0200

    translate find phone activity

--- src/com/veryfit/sdkdemo/ui/FindPhoneActivity.java
+++ src/com/veryfit/sdkdemo/ui/FindPhoneActivity.java
@@ -21,7 +21,6 @@ public class FindPhoneActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_findphone);
 		initView();
@@ -31,7 +30,6 @@ public class FindPhoneActivity extends BaseActivity {
 
 	@Override
 	public void initView() {
-		// TODO Auto-generated method stub
 		super.initView();
 		dialog = new BufferDialog(this);
 		switchFindPhone = (Switch) findViewById(R.id.switch_findphone);
@@ -39,47 +37,45 @@ public class FindPhoneActivity extends BaseActivity {
 
 	@Override
 	public void initData() {
-		// TODO Auto-generated method stub
 		super.initData();
-		// 初始化寻找手机开关数据
 		FindPhoneOnOff find = ProtocolUtils.getInstance().getFindPhone();
 		switchFindPhone.setChecked(find.getOnOff());
 	}
 
 	@Override
 	public void addListener() {
-		// TODO Auto-generated method stub
 		super.addListener();
 		switchFindPhone.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
 			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
-				// 每次设置成功后设置的数据会保存在数据库中,用于界面展示。
+				// The data set after each successful setting will be saved in the database for interface display.
 				dialog.show();
-				ProtocolUtils.getInstance().setFindPhone(arg1);//设置寻找手机开关
+				ProtocolUtils.getInstance().setFindPhone(arg1);
 			}
 		});
 	}
 
 	@Override
 	public void onSysEvt(int arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
 		super.onSysEvt(arg0, arg1, arg2, arg3);
-		DebugLog.d("收到寻找手机的命令");
+		DebugLog.d("Received a find phone request");
 		if (arg1 == ProtocolEvt.SET_CMD_FIND_PHONE.toIndex() && arg2 == ProtocolEvt.SUCCESS) {
 			mHandler.post(new Runnable() {
 				@Override
 				public void run() {
 					dialog.dismiss();
-					Toast.makeText(FindPhoneActivity.this, "寻找手机开关设置成功", Toast.LENGTH_LONG).show();
+					Toast.makeText(FindPhoneActivity.this, "Find phone setting set successfully", Toast.LENGTH_LONG).show();
 				}
 			});
-		}else if (arg1 == ProtocolEvt.BLE_TO_APP_FIND_PHONE_START.toIndex()) {//收到手环发送过来的寻找手机命令，客户端收到这条命令做相应处理，比如让手机响铃等操作
+		}else if (arg1 == ProtocolEvt.BLE_TO_APP_FIND_PHONE_START.toIndex()) {
+			// After receiving the mobile phone search command sent by the bracelet,
+			// the client receives this command and does the corresponding processing, such as making the mobile phone ring.
 			mHandler.post(new Runnable() {
 				@Override
 				public void run() {
 					dialog.dismiss();
-					Toast.makeText(FindPhoneActivity.this, "收到寻找手机的命令", Toast.LENGTH_LONG).show();
+					Toast.makeText(FindPhoneActivity.this, "Received a find phone request", Toast.LENGTH_LONG).show();
 				}
 			});
 		}
