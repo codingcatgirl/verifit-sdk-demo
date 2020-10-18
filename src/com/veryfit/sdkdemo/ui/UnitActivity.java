116aac601a9461b298c6c93ea0b7c38e9a98a8cc
Laura Klünder <laura@codingcatgirl.de>
2020-10-18 16:53:00 +0200

    translate unit activity

--- src/com/veryfit/sdkdemo/ui/UnitActivity.java
+++ src/com/veryfit/sdkdemo/ui/UnitActivity.java
@@ -37,7 +37,6 @@ public class UnitActivity extends BaseActivity implements OnCheckedChangeListene
 	
 	@Override
 	public void initView() {
-		// TODO Auto-generated method stub
 		super.initView();
 		dialog=new BufferDialog(this);
 		switchGongyingzhi=(Switch)findViewById(R.id.switch_unit_gongyingzhi);
@@ -48,20 +47,19 @@ public class UnitActivity extends BaseActivity implements OnCheckedChangeListene
 	
 	@Override
 	public void initData() {
-		// TODO Auto-generated method stub
 		super.initData();
 		Units units=ProtocolUtils.getInstance().getUnits();
 		if(units!=null){
-			//公制 METRIC
-			//英制 BRITISH
+			// METRIC
+			// BRITISH
 			gongyingzhi=units.getMode();
 			switchGongyingzhi.setChecked(units.getMode()==Constants.METRIC);
-			//LANGUAGE_ZH 中文
-			//LANGUAGE_EN 英文
+			//LANGUAGE_ZH
+			//LANGUAGE_EN
 			language=units.getLanguage();
 			switchLanguage.setChecked(units.getLanguage()==Constants.LANGUAGE_ZH);
-			//TIME_MODE_24 24小时制
-			//TIME_MODE_12 12小时制
+			//TIME_MODE_24
+			//TIME_MODE_12
 			timemode=units.getTimeMode();
 			switchTime.setChecked(units.getTimeMode()==Constants.TIME_MODE_24);
 		}
@@ -70,7 +68,6 @@ public class UnitActivity extends BaseActivity implements OnCheckedChangeListene
 	
 	@Override
 	public void addListener() {
-		// TODO Auto-generated method stub
 		super.addListener();
 		switchGongyingzhi.setOnCheckedChangeListener(this);
 		switchLanguage.setOnCheckedChangeListener(this);
@@ -79,16 +76,15 @@ public class UnitActivity extends BaseActivity implements OnCheckedChangeListene
 			
 			@Override
 			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
 				Userinfos info=ProtocolUtils.getInstance().getUserinfos();
-				int stride=0;//步长单位是根据身高与性别计算得出的
+				int stride=0;// The stride unit is calculated based on height and gender
 				if(info.getGender()==Constants.FEMALE){
 					stride=(int) (0.413*info.getHeight());
 				}else {
 					stride=(int) (0.415*info.getHeight());
 				}
 				dialog.show();
-				ProtocolUtils.getInstance().setUint(gongyingzhi,stride,language,timemode);//公英制，步长单位，语言，时间制式
+				ProtocolUtils.getInstance().setUint(gongyingzhi,stride,language,timemode);
 			}
 		});
 	}
@@ -96,7 +92,6 @@ public class UnitActivity extends BaseActivity implements OnCheckedChangeListene
 	
 	@Override
 	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
-		// TODO Auto-generated method stub
 		switch (arg0.getId()) {
 		case R.id.switch_unit_gongyingzhi:
 			gongyingzhi=arg1?Constants.METRIC:Constants.BRITISH;
@@ -115,14 +110,13 @@ public class UnitActivity extends BaseActivity implements OnCheckedChangeListene
 	
 	@Override
 	public void onSysEvt(int arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
 		super.onSysEvt(arg0, arg1, arg2, arg3);
 		if (arg1==ProtocolEvt.SET_CMD_UINT.toIndex()&& arg2==ProtocolEvt.SUCCESS) {
 			mHandler.post(new Runnable() {
 				@Override
 				public void run() {
 					dialog.dismiss();
-					Toast.makeText(UnitActivity.this, "单位设置成功", Toast.LENGTH_LONG).show();
+					Toast.makeText(UnitActivity.this, "Unit settings successfully set", Toast.LENGTH_LONG).show();
 				}
 			});
 		}
