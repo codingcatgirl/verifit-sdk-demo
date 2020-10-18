254bbe0649074a3603e008ab3715cff79e5275d7
Laura Klünder <laura@codingcatgirl.de>
2020-10-18 16:39:43 +0200

    translate send log activity

--- src/com/veryfit/sdkdemo/ui/SendLogActivity.java
+++ src/com/veryfit/sdkdemo/ui/SendLogActivity.java
@@ -18,7 +18,6 @@ public class SendLogActivity extends Activity implements OnClickListener, SendLo
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_send_log);
 		initView();
@@ -32,15 +31,14 @@ public class SendLogActivity extends Activity implements OnClickListener, SendLo
 
 	@Override
 	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
 		switch (arg0.getId()) {
 		case R.id.btn_send_log:
 			dialog.show();
 			Thread thread = new Thread(new Runnable() {
 				@Override
 				public void run() {
-					// TODO Auto-generated method stub
-					ProtocolUtils.getInstance().sendLog(SendLogActivity.this, SendLogActivity.this);//耗时操作（压缩文件）
+					// Time-consuming operations (compressed files)
+					ProtocolUtils.getInstance().sendLog(SendLogActivity.this, SendLogActivity.this);
 				}
 			});
 			thread.start();
@@ -53,7 +51,6 @@ public class SendLogActivity extends Activity implements OnClickListener, SendLo
 
 	@Override
 	public void onFinish() {
-		// TODO Auto-generated method stub
 		dialog.dismiss();
 	}
 }
